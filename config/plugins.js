112530be17444d77f41616fa17b6ded451dfb98d
chetan dhargalkar <chetan.dmc@gmail.com>
2025-01-29 09:50:31 +0530

    re install cloundary

--- config/plugins.js
+++ config/plugins.js
@@ -15,32 +15,32 @@ module.exports = () => ({
             allowedContentTypes: ['api::sitemap.sitemap'], // Allow linking to the Sitemap collection type
         },
     },
-    upload: {
-        provider: 'local',  // Make sure this is set to 'local' if not using a cloud provider
-        providerOptions: {
-            sizeLimit: 10000000, // Optional size limit for uploads
-        },
-    },
     // upload: {
-    //     // Update your cloudinary credentials here
-    //     config: {
-    //         provider: "local",
-    //         providerOptions: {
-    //             cloud_name: "dipjj6azr",
-    //             api_key: "553969573594997",
-    //             api_secret: "IcuzLpetdqW0qooidsaf6Z2T-k8",
-    //         },
-    //         actionOptions: {
-    //             upload: {
-    //                 folder: "strapi-uploads",
-    //                 use_filename: true,
-    //                 unique_filename: false,
-    //                 resource_type: "auto",
-    //             },
-    //             delete: {
-    //                 invalidate: true,
-    //             },
-    //         },
+    //     provider: 'local',  // Make sure this is set to 'local' if not using a cloud provider
+    //     providerOptions: {
+    //         sizeLimit: 10000000, // Optional size limit for uploads
     //     },
     // },
+    upload: {
+        // Update your cloudinary credentials here
+        config: {
+            provider: "cloudinary",
+            providerOptions: {
+                cloud_name: "dipjj6azr",
+                api_key: "553969573594997",
+                api_secret: "IcuzLpetdqW0qooidsaf6Z2T-k8",
+            },
+            actionOptions: {
+                upload: {
+                    folder: "strapi-uploads",
+                    use_filename: true,
+                    unique_filename: false,
+                    resource_type: "auto",
+                },
+                delete: {
+                    invalidate: true,
+                },
+            },
+        },
+    },
 });
