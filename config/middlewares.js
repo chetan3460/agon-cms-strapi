58190d270f84e441ca626f70d005339ae4dd5d68
chetan dhargalkar <chetan.dmc@gmail.com>
2025-01-30 10:34:39 +0530

    Fix: Update on cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,19 +6,18 @@ module.exports = [
     config: {
       contentSecurityPolicy: {
         directives: {
-          'connect-src': ["'self'", 'agon-nuxt.vercel.app'],
-          'img-src': ["'self'", 'data:', 'blob:', 'agon-cms-strapi.onrender.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'agon-cms-strapi.onrender.com'],
+          'connect-src': ["'self'", 'https://agon-nuxt.vercel.app'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://agon-cms-strapi.onrender.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://agon-cms-strapi.onrender.com'],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
   {
-    name: 'strapi::cors',
+    name: 'strapi::cors', // ✅ Remove `enabled: true`
     config: {
-      enabled: true,
-      origin: ['https://agon-nuxt.vercel.app'], // Allow frontend
+      origin: ['https://agon-nuxt.vercel.app'], // ✅ Add your frontend URL
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       credentials: true,
